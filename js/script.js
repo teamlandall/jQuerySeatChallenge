f79e15865d181f948f875c39703785709ba8f294
Landall Proctor <landallproctor@gmail.com>
2015-09-10 10:11:21 -0400

    Submit button on Form changes seat color and disables it.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	//display form
 
      $(".btn").click(function(){
+        $(this).attr('id', 'active');
          if($("#seat-form").is(":visible")){
            $("#seat-form").hide();
            $("#seat-form")[0].reset();
@@ -12,6 +13,12 @@ $(document).ready(function() {
         return false;
      });
 
+     $("#submit").click(function(){
+        $('#active').addClass('btn-danger').attr('disabled', 'disabled');
+        $('#active').text('Reserved');
+        // $('.active').removeClass('btn btn-primary btn-sm').addClass('btn btn-danger btn-sm');   
+        $('#submit').addClass('btn btn-default');
+     });
 
 
 });
