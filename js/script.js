09eb644bcca1815bc4bad4d19f603fca394f8637
Angeline <ammeitzler@gmail.com>
2015-09-09 22:37:54 -0400

    form text field resets.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 
 	//display form
-	
+
      $(".btn").click(function(){
          if($("#seat-form").is(":visible")){
            $("#seat-form").hide();
+           $("#seat-form")[0].reset();
         } else {
            $("#seat-form").show();
         }
         return false;
      });
+
+
 
 });
 
