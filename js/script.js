6501790b78b269ef1f1cebfcf0ab744ffa964e65
Landall Proctor <landallproctor@gmail.com>
2015-09-11 16:26:16 -0400

    Simplified form drop down. Still working on conditional logic.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
 $(document).ready(function() {
 
 	// display form
-
-     $(".btn").click(function(){
-        $(this).attr('id', 'active');
-         if($("#seat-form").is(":visible")){
-           $("#seat-form").hide();
-           $("#seat-form")[0].reset();
-        } else {
-           $("#seat-form").show();
-        } 
-          $(".btn-sm").each(function(){
-              $(".btn-sm").attr("disabled", "disabled");
-            });
-            $(this).removeAttr("disabled", "disabled");
-        return false;
-      });
+  $(".btn").click(function(){
+    $(this).attr('id', 'active');
+    $('#seat-form').slideDown();
+  });
 
   // submit button
-
-     $("#submit").click(function(){
-        $(this).removeAttr('id', 'active');
-        $('#active').addClass('btn-danger').text('Reserved'); 
-
+  $("#submit").click(function(){
+    event.preventDefault();
+    $(this).removeAttr('id', 'active');
+    $('#seat-form').slideUp();
+    $('#active').addClass('btn-danger').text('Reserved');
         
-        $(".btn-sm").each(function(){
-          $(".btn-sm").removeAttr("disabled", "disabled");
+        //below uses replace with to inject new html with danger class for color.
+        // $('#active').replaceWith('<button type="button" class="btn btn-primary btn-sm btn-danger">Reserved</button>'); 
+
+        //this doens't work. figure this out.
+        $("button").each(function(){
+          if($('button').class() !== 'reserved'){
+            $('.btn-sm').removeAttr('disabled', 'disabled');
+          } else{
+            $('.btn-sm').attr('disabled', 'disabled');
+          }
         });
 
-        $('#active').attr("disabled", "disabled").removeAttr("id", "active");
-        $('#submit').addClass('btn btn-default');
-     });
+    $('#active').removeAttr("id", "active");
+    $('#submit').addClass('btn btn-default');
+    });
 
 });
  
